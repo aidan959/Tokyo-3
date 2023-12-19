42757f9588272d01ddd251cf3be759a25a5296f7
mattweedy <matthewtweedy02@gmail.com>
2023-12-19 23:13:52 +0000

    swapped camera for shakeable camera - TODO cam height and collision

--- player.gd
+++ player.gd
@@ -19,7 +19,9 @@ var walk_vel: Vector3
 var grav_vel: Vector3
 var jump_vel: Vector3
 
-@onready var camera: Camera3D = $Camera
+#@onready var camera: Camera3D = $Camera
+#@onready var camera: Area3D = $shakeable_camera
+@onready var camera := $shakeable_camera as Area3D
 
 func _ready() -> void:
 	capture_mouse()
--- shakeable_camera.gd
+++ shakeable_camera.gd
@@ -13,7 +13,7 @@ var trauma := 0.0
 
 var time := 0.0
 
-@onready var camera := $Camera as Camera3D
+@onready var camera := $Camera3D as Camera3D
 @onready var initial_rotation := camera.rotation_degrees as Vector3
 
 func _ready():
@@ -21,14 +21,14 @@ func _ready():
 
 func _process(delta):
 	time += delta
-	trauma = max(trauma - delta*trauma_reduction_rate, 0.0)
+	trauma = max(trauma - delta * trauma_reduction_rate, 0.0)
 	
 	camera.rotation_degrees.x = initial_rotation.x + max_x * get_shake_intensity() * get_noise_from_seed(0)
 	camera.rotation_degrees.y = initial_rotation.y + max_y * get_shake_intensity() * get_noise_from_seed(1)
 	camera.rotation_degrees.z = initial_rotation.z + max_z * get_shake_intensity() * get_noise_from_seed(2)
 
 func add_trauma(trauma_amount : float):
-	trauma = clamp(trauma + trauma, 0.0, 1.0)
+	trauma = clamp(trauma + trauma_amount, 0.0, 1.0)
 
 func get_shake_intensity() -> float:
 	return trauma*trauma
